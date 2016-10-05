240f67cf5bae75cd0b97e8fcbd1ce0a4a69eb4d7
SAMUEL <samuelbd>
2016-10-05 13:34:21 -0500

    Login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -13,13 +13,48 @@ class login extends CI_Controller {
 		$this->load->database('default');
 		$this->load->model('login_model');
 
-		//$usuario= $this->login_model->loguear($this->input->post("nombre"),$this->input->post("contra"));
+		$tipo = $_POST['opciones'];
 
-		//if(count($usuario) == 1){			
-		if(true){		
-			redirect(base_url().'uifisi','refresh');
-		}else{
-			redirect(base_url(),'refresh');
+		if ($tipo == "docente") {
+			$usuario = $this->login_model->logueardocente($this->input->post("nombre"),$this->input->post("contra"));
+
+			if(count($usuario) == 1){			
+				if(true){		
+					redirect(base_url().'uifisi','refresh');
+				}
+			}
+			else{
+				echo "<center> <span aria-hidden='true'></span> <b> El Docente no existe </b> </center>";
+				redirect(base_url(),'refresh');
+			}
+		}
+
+		if ($tipo == "estudiante") {
+
+			$alumno = $this->login_model->loguearalumno($this->input->post("nombre"),$this->input->post("contra"));
+
+			if(count($alumno) == 1){			
+				if(true){		
+					redirect(base_url().'uifisi','refresh');
+				}
+			}
+			else{
+				echo "<center> <span class='modal hide' aria-hidden='true'></span> <b> El Estudiante no existe </b> </center>";
+
+				echo "<div id='AlertaLogin' class='modal hide'>
+	                    <div class='modal-header'>
+	                        <button data-dismiss='modal' class='close' type='button'>×</button>
+	                        <h3>Alert modal</h3>
+	                    </div>
+
+	                    <div class='modal-body'>
+	                        <center> <span aria-hidden='true'></span> <b> El Usuario no Existe </b> </center>
+	                    </div>
+
+	                    <div class='modal-footer'> <a data-dismiss='modal' class='btn btn-primary' href='#''>Confirmar</a></div>
+	                </div>";
+				redirect(base_url(),'refresh');
+			}
 		}
 	}
 
