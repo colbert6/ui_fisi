d6f266455716b1ae9f7e31d9cf8dd074cee2b37f
Jhoan Manuel Zarria Sinarahua <jhoan@lighthammer.jhoan>
2016-11-22 16:51:37 -0500

    word completo

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'postgres';
-$db['default']['password'] = 'postgres';
+$db['default']['password'] = '123';
 $db['default']['database'] = 'ui_fisi';
 $db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
